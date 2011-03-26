28f30e356e24412eb52dcb2049ba16cd165e0446
olifante <trinosauro@gmail.com>
2011-03-26 17:43:49 +0000

    set view controller to be root view controller

--- HelloGoodbyeUniv/HelloGoodbyeUnivAppDelegate.m
+++ HelloGoodbyeUniv/HelloGoodbyeUnivAppDelegate.m
@@ -16,7 +16,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    self.window.rootViewController = self.viewController;
+    [self.window addSubview:self.viewController.view];
     [self.window makeKeyAndVisible];
     return YES;
 }
