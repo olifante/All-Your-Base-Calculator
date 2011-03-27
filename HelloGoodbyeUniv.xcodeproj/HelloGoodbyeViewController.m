c66ca5818ae5d10b754b24888f8a77e2a23531f5
olifante <trinosauro@gmail.com>
2011-03-27 03:13:35 +0100

    now able to use result obtained from "+" as first operand to a binary operation

--- HelloGoodbyeUniv.xcodeproj/HelloGoodbyeViewController.m
+++ HelloGoodbyeUniv.xcodeproj/HelloGoodbyeViewController.m
@@ -63,6 +63,9 @@
 {
     NSString *operation = sender.titleLabel.text;
     NSLog(@"%@ operation pressed", operation);
+    if ([self.currentDigits isEqualToString:@""] && self.previousDigits) {
+        self.currentDigits = self.previousDigits;
+    }
     if (self.pendingOperation) {
         NSString *resultDigits = [self performPendingOperation];
         if ([operation isEqualToString:@"="]) {
