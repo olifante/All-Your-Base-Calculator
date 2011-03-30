2f7651158d0f9aee07fea45a328ccdc83bd8ec0d
olifante <trinosauro@gmail.com>
2011-03-30 21:36:46 +0100

    add LogicTests target for unit testing

---
+++ LogicTests/LogicTests.h
@@ -0,0 +1,16 @@
+//
+//  LogicTests.h
+//  LogicTests
+//
+//  Created by Tiago Henriques on 3/30/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LogicTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ LogicTests/LogicTests.m
@@ -0,0 +1,33 @@
+//
+//  LogicTests.m
+//  LogicTests
+//
+//  Created by Tiago Henriques on 3/30/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import "LogicTests.h"
+
+
+@implementation LogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LogicTests");
+}
+
+@end
