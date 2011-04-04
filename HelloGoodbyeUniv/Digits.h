93e3916a5f6589f6bde068226a128b638e8cccc0
olifante <trinosauro@gmail.com>
2011-04-04 16:06:07 +0100

    added class Digits

---
+++ HelloGoodbyeUniv/Digits.h
@@ -0,0 +1,16 @@
+//
+//  Digits.h
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/4/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Digits : NSObject {
+    
+}
+
+@end
---
+++ HelloGoodbyeUniv/Digits.m
@@ -0,0 +1,14 @@
+//
+//  Digits.m
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/4/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import "Digits.h"
+
+
+@implementation Digits
+
+@end
