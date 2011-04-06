b3dbfef1bd47269dc306f8be3ed891c2f861bbfa
olifante <trinosauro@gmail.com>
2011-04-06 12:28:14 +0100

    adedd RationalDigits

---
+++ HelloGoodbyeUniv/RationalDigits.h
@@ -0,0 +1,16 @@
+//
+//  RationalDigits.h
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/6/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Digits.h"
+
+@interface RationalDigits : Digits {
+    
+}
+
+@end
---
+++ HelloGoodbyeUniv/RationalDigits.m
@@ -0,0 +1,14 @@
+//
+//  RationalDigits.m
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/6/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import "RationalDigits.h"
+
+
+@implementation RationalDigits
+
+@end
