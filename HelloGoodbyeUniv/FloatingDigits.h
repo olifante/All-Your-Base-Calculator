2b9b5d9ceca83c22830de8a90c9184e045577c24
olifante <trinosauro@gmail.com>
2011-04-06 12:33:29 +0100

    added FloatingDigits

---
+++ HelloGoodbyeUniv/FloatingDigits.h
@@ -0,0 +1,16 @@
+//
+//  FloatingDigits.h
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/6/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Digits.h"
+
+@interface FloatingDigits : Digits {
+    
+}
+
+@end
---
+++ HelloGoodbyeUniv/FloatingDigits.m
@@ -0,0 +1,14 @@
+//
+//  FloatingDigits.m
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/6/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#import "FloatingDigits.h"
+
+
+@implementation FloatingDigits
+
+@end
