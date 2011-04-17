1a35fa0cbd8c92b2f1767423505d54ffdbe6151e
olifante <trinosauro@gmail.com>
2011-04-17 01:06:12 +0100

    added itoa implementation from K&R2; added modified copy of standard iPhone view

---
+++ HelloGoodbyeUniv/itoa.h
@@ -0,0 +1,12 @@
+//
+//  itoa.h
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/17/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#include <string.h>
+
+void reverse(char s[]);
+void itoa(int n, char s[]);
