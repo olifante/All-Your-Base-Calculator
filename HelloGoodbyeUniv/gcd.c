7a261f52304738d3021185d7b0a37043b4cf0714
olifante <trinosauro@gmail.com>
2011-04-19 15:21:05 +0100

    added gcd.c

---
+++ HelloGoodbyeUniv/gcd.c
@@ -0,0 +1,16 @@
+//
+//  gcd.c
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/19/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+#include "gcd.h"
+
+/* gcd: compute greatest common divisor between p and q */
+int gcd(int p, int q) {
+    int	r;
+    if ((r = p%q) == 0) return q;
+    else return gcd(q, r);
+}
---
+++ HelloGoodbyeUniv/gcd.h
@@ -0,0 +1,9 @@
+//
+//  gcd.h
+//  AllYourBase
+//
+//  Created by Tiago Henriques on 4/19/11.
+//  Copyright 2011 notknot. All rights reserved.
+//
+
+int gcd(int p, int q);
