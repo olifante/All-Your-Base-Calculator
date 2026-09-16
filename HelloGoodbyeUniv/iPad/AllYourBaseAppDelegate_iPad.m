870d61765fbd38851adc056ac20601fd6af6c78c
Tiago Castro Henriques <tiago@notknot.com>
2026-09-16 19:02:32 +0100

    Remove diagnostics

--- HelloGoodbyeUniv/iPad/AllYourBaseAppDelegate_iPad.m
+++ HelloGoodbyeUniv/iPad/AllYourBaseAppDelegate_iPad.m
@@ -13,7 +13,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSLog(@"DIAG iPad didFinishLaunchingWithOptions called, self=%@ self.window(before)=%@", self, self.window);
     AllYourBaseModel *theModel = [[AllYourBaseModel alloc] init];
     
     NSMutableArray *vcs = [[NSMutableArray alloc] init];
